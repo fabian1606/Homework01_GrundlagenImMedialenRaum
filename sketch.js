ac291991a9a1c98bf79f341759bd29fb1a6e4926
jens <115379514+InfernisVox@users.noreply.github.com>
2023-03-26 03:01:02 +0200

    made the rgb hsl and hex of copyable

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
-// const { color } = require("echarts");
-
-let objects = [];
-let size = 150;
-let ballcolor;
-let currenthsb;
+const objects = [];
+const size = 150;
+let ballColor;
+let currentHSB;
+let rgbInput, hexInput, hslInput;
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
 	colorMode(HSB, 360, 100, 100, 1);
-	currenthsb = color(360, 100, 100);
+	currentHSB = color(360, 100, 100);
 
-	// create a grid of objects filling the canvas with the same gap between them on the x and y axis
 	for (let x = 5; x < size - 5; x++) {
 		for (let y = height / 90; y < size / 1.9; y++) {
 			objects.push(
@@ -19,108 +17,118 @@ function setup() {
 		}
 	}
 
-	// for (let x = 0; x < window.innerWidth / 40; x++) {
-	// 	for (let y = 0; y < 60; y++) {
-	// 		objects.push(
-	// 			new MyObject((width / size) * x, y * (height / size), size)
-	// 		);
-	// 	}
-	// }
+	ballColor = color(20, 80, 100);
 
-	ballcolor = color(20, 80, 100);
+	// create input fields
+	rgbInput = createInput();
+	rgbInput.position(50, objects[objects.length - 1].myY + 50);
+
+	hexInput = createInput();
+	hexInput.position(250, objects[objects.length - 1].myY + 50);
+
+	hslInput = createInput();
+	hslInput.position(450, objects[objects.length - 1].myY + 50);
+
+	// set input fields to readonly
+	rgbInput.attribute("readonly", "");
+	hexInput.attribute("readonly", "");
+	hslInput.attribute("readonly", "");
 }
 
 function draw() {
 	background(0);
-	objects.forEach((obj) => {
-		obj.display(mouseincanvas());
-		// obj.setColor(ballcolor)
+	objects.forEach((object) => {
+		object.display(mouseInCanvas());
 	});
 
-	printtext();
+	printText();
 }
 
-function mouseincanvas() {
+function mouseInCanvas() {
 	return mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height;
 }
+
 function mousePressed() {
-	for (let index = 0; index < objects.length; index++) {
-		const obj = objects[index];
-		// obj.setColor(color(map(index%size,0,size,0,360),map(parseInt(index/size),0,size,0,225),100));
-		// console.log(map(index%size,0,size,0,360));
-
-		//obj.setColor(color(int(map(index / size, 0, size, 0, 255)), 100, 100));
-
-		//create a hsb gradient with the objects in the array
-		obj.setColor(
+	for (let i = 0; i < objects.length; i++) {
+		const object = objects[i];
+		object.setColor(
 			color(
-				int(map(index, 0, objects.length, 0, 360)),
-				int(map(obj.myY, 0, height, 10, 100)),
+				int(map(i, 0, objects.length, 0, 360)),
+				int(map(object.myY, 0, height, 10, 100)),
 				100
 			)
 		);
-		//let the saturation of the color change with the object position on the y axis
-
-		// obj.setColor(
-		// 	color(int(map(index, 0, objects.length, 0, 360)), int(map(obj.myY, 0, height, 0, 100)), 100)
 	}
 }
+
 function mouseReleased() {
-	for (let index = 0; index < objects.length; index++) {
-		const obj = objects[index];
-		currenthsb = color(
+	for (let i = 0; i < objects.length; i++) {
+		const object = objects[i];
+		currentHSB = color(
 			int(map(mouseX, 0, width, 0, 360)),
 			int(map(mouseY, 0, height, 10, 100)),
 			100
 		);
-		obj.setColor(currenthsb);
+		object.setColor(currentHSB);
 	}
 }
 
-function printtext() {
-	let printedcolor = currenthsb.toString();
-	printedcolor = printedcolor.replace("rgba(", "");
-	printedcolor = printedcolor.replace(",1)", "");
-	printedcolor = printedcolor.split(",");
-	console.log(printedcolor);
+function printText() {
+	const printedColor = currentHSB
+		.toString()
+		.replace("rgba(", "")
+		.replace(",1)", "")
+		.split(",");
 
 	textSize(20);
 	text("click to change color", 50, objects[1].myY - 40);
-	text("RGB: " + printedcolor, 50, objects[objects.length - 1].myY + 40);
-	text(
-		"HEX: " + rgbToHex(printedcolor[0], printedcolor[1], printedcolor[2]),
-		250,
-		objects[objects.length - 1].myY + 40
+
+	// set text of input fields
+	rgbInput.value(printedColor.join(", "));
+	hexInput.value(rgbToHex(printedColor[0], printedColor[1], printedColor[2]));
+	hslInput.value(
+		rgbToHsl(printedColor[0], printedColor[1], printedColor[2]).join(", ")
 	);
-	text(
-		"HSL: " + rgbToHsl(printedcolor[0], printedcolor[1], printedcolor[2]),
-		430,
-		objects[objects.length - 1].myY + 40
-	);
+
+	text("RGB:", 50, objects[objects.length - 1].myY + 40);
+	text("HEX:", 250, objects[objects.length - 1].myY + 40);
+	text("HSL:", 450, objects[objects.length - 1].myY + 40);
 }
 
-function rgbToHex(r, g, b) {
+// Convert RGB color values to a hexadecimal color code.
+const rgbToHex = (r, g, b) => {
 	return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
-}
+};
 
-function rgbToHsl(r, g, b) {
+// Convert RGB color values to HSL color values.
+const rgbToHsl = (r, g, b) => {
+	// Convert RGB values from 0-255 to 0-1.
 	r /= 255;
 	g /= 255;
 	b /= 255;
-	const l = Math.max(r, g, b);
-	const s = l - Math.min(r, g, b);
-	const h = s
-		? l === r
-			? (g - b) / s
-			: l === g
-			? 2 + (b - r) / s
-			: 4 + (r - g) / s
-		: 0;
-	return [
-		int(60 * h < 0 ? 60 * h + 360 : 60 * h),
-		int(
-			100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0)
-		),
-		int((100 * (2 * l - s)) / 2),
-	];
-}
+
+	const max = Math.max(r, g, b);
+	const min = Math.min(r, g, b);
+	const l = (max + min) / 2;
+
+	if (max === min) {
+		// Grayscale.
+		return [0, 0, Math.round(l * 100)];
+	} else {
+		const d = max - min;
+		const s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		let h;
+		switch (max) {
+			case r:
+				h = ((g - b) / d + (g < b ? 6 : 0)) / 6;
+				break;
+			case g:
+				h = ((b - r) / d + 2) / 6;
+				break;
+			case b:
+				h = ((r - g) / d + 4) / 6;
+				break;
+		}
+		return [Math.round(h * 360), Math.round(s * 100), Math.round(l * 100)];
+	}
+};
